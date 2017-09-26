75cd09b7861b235d2adb7d79105f05295dd63a34
Ansarul14 <ansarul.islam117@gmail.com>
2017-09-27 01:49:59 +0600

    Add files via upload

---
+++ 841B.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+   int c, sum,n,i;
+   ll m;
+    scanf("%d",&n);
+
+        for(i=1;i<=n;i++)
+        {
+            //cin>>m;
+            scanf("%lld",&m);
+            //cin>>arr[i];
+            if (m%2!=0){printf("First\n");
+           return 0;}
+        }
+
+            //cout<<"Second"<<endl;
+            printf("Second\n");
+
+}
